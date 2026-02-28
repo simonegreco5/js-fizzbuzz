d0fdf893e81e560a9f57d9a95df15b666cb6b537
simonegreco5 <simoxey.greco@gmail.com>
2026-02-28 13:05:28 +0100

    add code in js (conditions)

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // iniziamo ad impostare il ciclo per stampare i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    
+
+    // condition multiple of 3
+    if (i % 3 === 0) {
+        console.log('Fizz')
+
+    // condition multiple of 5
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+
+
 }
